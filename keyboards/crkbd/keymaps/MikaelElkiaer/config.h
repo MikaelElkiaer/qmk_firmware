564f7f4def8bfc70ac21acc8ff63dc3a3c57f23f
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2021-07-07 14:28:10 +0200

    Swapped tapping force hold for oneshot tap toggle.

--- keyboards/crkbd/keymaps/MikaelElkiaer/config.h
+++ keyboards/crkbd/keymaps/MikaelElkiaer/config.h
@@ -30,8 +30,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 5000
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
