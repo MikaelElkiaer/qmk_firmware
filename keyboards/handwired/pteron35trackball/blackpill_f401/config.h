8c36cc0337a05c4373d58681cd9b90e6e2f5c1ee
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-02-13 23:29:12 +0100

    Added handwired pteron35, trackball pending.

---
+++ keyboards/handwired/pteron35trackball/blackpill_f401/config.h
@@ -0,0 +1,24 @@
+/* Copyright 2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define MATRIX_COL_PINS \
+    { A7, B0, B1, B2, B10, B12, B13, B14, B15, A8 }
+#define MATRIX_ROW_PINS \
+    { A6, A5, A4, A3 }
+#define UNUSED_PINS \
+    { A2, A1, A0, C15, C14, C13, A9, A10, A11, A12, A15, B3, B4, B5, B8, B9 }
